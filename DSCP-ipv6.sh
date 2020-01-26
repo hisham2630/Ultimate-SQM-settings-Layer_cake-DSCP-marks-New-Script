635e3c0095075ca169b6b2c9496462c963c9b255
Guillaume DELVIT <guiguid@free.fr>
2020-01-26 20:55:00 +0100

    remove veth stuff

--- DSCP-ipv6.sh
+++ DSCP-ipv6.sh
@@ -1,31 +1,18 @@
 #!/bin/bash -x
 IPT="iptables"
 IPT6="ip6tables"
-###########
-#Veth start
-###########
-WANIF="pppoe-wan" #wan interface name
 
-tc qdisc add dev wlan0 root mq #setup multi queue for wifi device
-## set up veth devices to handle inbound and outbound traffic
-ip link show | grep veth0 || ip link add type veth
+##########
+# This file can be used directly in Firewall - Custom Rules 
+##########
 
-## get new veth interfaces up
-ip link set veth0 up
-ip link set veth1 up
+##########
+# Veth 
+# if need remove "#"
+# or copy and paste veth.sh file bellow
+##########
+#./veth.sh
 
-## trun on promisc mode,sometimes it's needed to make bridge work
-ip link set veth1 promisc on
-
-## add veth1 to bridge
-brctl addif br-lan veth1
-
-## just to make sure there's nothing inside this table
-ip rule del priority 100
-ip route flush table 100
-#########
-#Veth end
-#########
 
 ##ipset for streaming sites.they are being filled by dnsmasq
 ipset create streaming hash:ip
